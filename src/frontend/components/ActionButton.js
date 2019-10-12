ae6cc217de1c28db8f65e819e4a9f2101ae15856
luandro <luandro@gmail.com>
2019-10-12 09:57:48 -0300

    changed ActionButton to speaking icon

--- src/frontend/components/ActionButton.js
+++ src/frontend/components/ActionButton.js
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { Image } from 'react-native'
 import FAB from 'react-native-action-button'
 
 export default ({ action }) => (
@@ -10,5 +11,15 @@ export default ({ action }) => (
     }}
     buttonColor='rgba(231,76,60,1)'
     onPress={action}
+    renderIcon={active => (
+      <Image
+        source={require('../assets/speak.png')}
+        style={{
+          height: 40,
+          width: 24,
+          marginLeft: 5
+        }}
+      />
+    )}
   />
 )
