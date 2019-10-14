c7555bdf541ce49bbbef9341fb6f424efa59844b
luandro <luandro@gmail.com>
2019-10-14 16:08:03 -0300

    initialized TrackPlayer handler

--- index.js
+++ index.js
@@ -2,8 +2,12 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './src/frontend/components/App';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native'
+import App from './src/frontend/components/App'
+import { name as appName } from './app.json'
+import TrackPlayer from 'react-native-track-player'
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => App)
+TrackPlayer.registerPlaybackService(() =>
+  require('./src/frontend/lib/player-handler')
+)
